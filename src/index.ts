d653292cd0c59b2d07e062b04dbe8542707628ab
Luizhnrs <Luizhnrsg@gmail.com>
2024-05-28 15:21:50 -0300

    Feat: User Schema created and implemented, and some packages of mongoose are added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -16,3 +17,13 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer();
+
+server.listen(3000, () => {
+  console.log("Server now is running on http://localhost:3000/")
+});
+
+const MONGO_URL = 'mongodb+srv://Luiz:11072001@users.ulbesqb.mongodb.net/?retryWrites=true&w=majority&appName=users';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error:Error) => console.log(error));
